9f4af02b77e35b390b3aae71710a707578f6cb3b
unknown <23214290062@academico.iesb.br>
2024-09-05 21:28:33 -0300

    Atividade

--- Sorteio/components/RandomNumber.js
+++
@@ -1,8 +0,0 @@
-const getRandomNumber = () => Math.floor(Math.random() * 61);
-
-const RandomNumber = () => {
-  const number = getRandomNumber();
-  return <span style={{ fontSize: '24px', margin: '0 10px' }}>{number}</span>;
-};
-
-export default RandomNumber;
